332e90d9c931efe3368b2692674fa1026a8e7538
Nick Rose <nicholas.rose@cbc.ca>
2019-11-30 18:52:08 -0500

    Get rid of consumer for app database, just use Provider.of

--- lib/main.dart
+++ lib/main.dart
@@ -16,61 +16,64 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Consumer<AppDatabase>(
-          builder:
-              (BuildContext context, AppDatabase appDatabase, Widget child) {
-            return Scaffold(
-              body: StreamProvider<List<Post>>(
-                create: (BuildContext context) {
-                  return appDatabase.watchPosts();
-                },
-                child: Consumer<List<Post>>(
-                  builder:
-                      (BuildContext context, List<Post> posts, Widget child) {
-                    return ListView.builder(
-                      itemCount: posts?.length ?? 0,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(posts[index].title),
-                          subtitle: Text(posts[index].body),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
-              floatingActionButton: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  FloatingActionButton(
-                    child: Icon(Icons.close),
-                    onPressed: () {
-                      appDatabase.deleteAll();
-                      id = 1;
-                    },
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  FloatingActionButton(
-                    child: Icon(Icons.add),
-                    onPressed: () {
-                      appDatabase.insert(
-                        Post(
-                          userId: id * id,
-                          id: id,
-                          title: "Number $id",
-                          body: "Body $id",
-                        ),
-                      );
-                      id++;
-                    },
-                  ),
-                ],
-              ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appDatabase = Provider.of<AppDatabase>(context);
+
+    return Scaffold(
+      body: StreamProvider<List<Post>>(
+        create: (BuildContext context) {
+          return appDatabase.watchPosts();
+        },
+        child: Consumer<List<Post>>(
+          builder: (BuildContext context, List<Post> posts, Widget child) {
+            return ListView.builder(
+              itemCount: posts?.length ?? 0,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(posts[index].title),
+                  subtitle: Text(posts[index].body),
+                );
+              },
             );
           },
         ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(Icons.close),
+            onPressed: () {
+              appDatabase.deleteAll();
+              id = 1;
+            },
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              appDatabase.insert(
+                Post(
+                  userId: id * id,
+                  id: id,
+                  title: "Number $id",
+                  body: "Body $id",
+                ),
+              );
+              id++;
+            },
+          ),
+        ],
       ),
     );
   }
